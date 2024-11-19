71f177e3ee25a08b2db7346bc91cfffc02b4ad90
Joseph Senecal <jasenecal2@gmail.com>
2024-11-19 11:37:31 -0700

    Add login and logout routes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -24,6 +24,8 @@ def register():
         
         if error is None:
             try:
+                # the db library should take care of escaping characters to
+                # avoid SQL injection attacks here. Neat!
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
@@ -38,3 +40,43 @@ def register():
     
     return render_template('auth/register.html')
 
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM users WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Login failed, please try again"
+        elif not check_password_hash(user['password'], password):
+            error = "Login failed, please try again"
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM users WHERE id = ?', (user_id,)
+        )
