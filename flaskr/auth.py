dbbca75d33d059d440c5262981bdfd9e95238dcf
Joseph Senecal <jasenecal2@gmail.com>
2024-11-19 12:08:22 -0700

    Fix bad table name

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -48,7 +48,7 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM users WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
@@ -78,7 +78,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM users WHERE id = ?', (user_id,)
+            'SELECT * FROM user WHERE id = ?', (user_id,)
         )
 
 def login_required():
